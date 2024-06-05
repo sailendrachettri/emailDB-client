a06ad841605fad1ce23864b9133473c862fabb21
sailendrachettri <sailendra9083@gmail.com>
2024-06-05 22:15:10 +0530

    production - version 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const SERVER_URI = "http://localhost:5001";
+// export const SERVER_URI = "http://localhost:5000"; // development
+export const SERVER_URI = "https://emaildb-server.onrender.com"; // production
