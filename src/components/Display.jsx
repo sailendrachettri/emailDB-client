ca62ebefae75263db2cb82e60367928f18f5003e
sailendrachettri <sailendra9083@gmail.com>
2024-06-10 22:28:37 +0530

    small bug fixes

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -41,8 +41,8 @@ const Display = () => {
                     <tr>
                         <th scope="col">S.No</th>
                         <th scope="col">Company</th>
-                        <th scope="col">Location</th>
                         <th scope="col">Type</th>
+                        <th scope="col">Location</th>
                         <th scope="col">Email 1</th>
                         <th scope="col">Email 2</th>
                         <th scope="col">Email 3</th>
